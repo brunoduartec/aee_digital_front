685e42edf8678af335af7436c91451d7d46d25a2
Bruno Duarte Corrêa <bruno.duartec@gmail.com>
2020-09-03 16:54:08 -0300

    add nome curto

--- src/app/util/consulta-centros.enum.ts
+++ src/app/util/consulta-centros.enum.ts
@@ -1,9 +1,9 @@
 export enum TipoConsultaCentro {
-    Nome = 1,
-    Regional = 2,
-    Endereço = 3,
-    Cidade = 4,
-    Estado = 5,
-    País = 6
-  }
-  
+  Nome = 1,
+  Regional = 2,
+  Endereço = 3,
+  Cidade = 4,
+  Estado = 5,
+  País = 6,
+  "Nome Curto" = 7
+}
