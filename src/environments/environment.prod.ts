1a4e36bc869546ddcb4bb7b0916b54bf79c59b26
Bruno Duarte Corrêa <bruno.duartec@gmail.com>
2020-10-21 18:56:41 -0300

    add trabalhosApi to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  centrosApi: 'http://162.214.123.133:3000/api/v1'
+  centrosApi: 'http://162.214.123.133:3000/api/v1',
+  trabalhosApi: 'http://162.214.123.133:3100/api/v1'
 };
