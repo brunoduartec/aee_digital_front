2bdeb369b5c0b90300e77235b0da509c7ef38ac5
Bruno Duarte <bruno.duartec@gmail.com>
2021-08-14 13:02:49 -0300

    update auth strategy

--- server.js
+++ server.js
@@ -13,8 +13,11 @@ const trabalhoscontroller = new trabalhosController();
 const authController = require("./controllers/auth.controller");
 const authcontroller = new authController();
 
-const SearchController = require("./controllers/search.controller")
-const searchcontroller = new SearchController(regionalcontroller,trabalhoscontroller);
+const SearchController = require("./controllers/search.controller");
+const searchcontroller = new SearchController(
+  regionalcontroller,
+  trabalhoscontroller
+);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -29,69 +32,138 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+async function TryAuthenticate(req, res, route) {
+  const loginInfo = {
+    user: req.body.email,
+    pass: req.body.pass,
+  };
+
+  req.body.route = req.originalUrl;
+  if (!req.session.loggedin) {
+    const auth = await authcontroller.authenticate(loginInfo);
+    if (!auth) {
+      res.render("pages/login", { auth: false, ...loginInfo });
+      return false;
+    } else {
+      req.session.loggedin = true;
+      return true;
+    }
+  } else {
+    return true;
+  }
+}
+
 app.get("/", async function (req, res) {
-  await authcontroller.authenticate(req,res,'pages/index');
+  if (await TryAuthenticate(req, res)) {
+    res.render("pages/index");
+  }
+});
+
+app.get("/centro", async function (req, res) {
+  if (await TryAuthenticate(req, res)) {
+    const centro = req.query.nome;
+    const edit = req.query.edit;
+    const option = "Centro";
+
+    const pesquisaInfo = {
+      search: centro,
+      option: option,
+    };
+    const result = await searchcontroller.getPesquisaResult(pesquisaInfo);
+
+    if (edit) {
+      res.render("pages/editar_centro", { result: result });
+    } else {
+      res.render("pages/detalhe", {
+        opcao: option,
+        result: result,
+      });
+    }
+  }
+});
+
+app.post("/update_centro", async function (req, res) {
+  if (await TryAuthenticate(req, res)) {
+    const centroInfo = {
+      NOME_CENTRO: req.body.nome,
+      NOME_CURTO: req.body.NOME_CURTO,
+      CNPJ_CENTRO: req.body.cnpj,
+      DATA_FUNDACAO: req.body.fundacao,
+      ENDERECO: req.body.endereco,
+      CEP: req.body.cep,
+      BAIRRO: req.body.bairro,
+      CIDADE: req.body.cidade,
+      ESTADO: req.body.estado,
+      PAIS: req.body.pais,
+    };
+
+    res.render("pages/detalhe", {
+      opcao: option,
+      result: result,
+    });
+  }
 });
 
 app.get("/login", async function (req, res) {
-  await authcontroller.authenticate(req,res,'pages/login');
+  if (await TryAuthenticate(req, res)) {
+    const route = req.body.route;
+    res.render("pages/index");
+  }
 });
 
 app.post("/login", async function (req, res) {
-  console.log("POST");
-  await authcontroller.authenticate(req,res,'pages/index');
+  if (await TryAuthenticate(req, res)) {
+    const route = req.body.route;
+    // res.redirect(route);
+    res.render("pages/index");
+  }
 });
 
 app.get("/pesquisar", async function (req, res) {
-  const regionais = await regionalcontroller.getRegionais();
-  const atividades = await trabalhoscontroller.getAtividades();
+  if (await TryAuthenticate(req, res)) {
+    const regionais = await regionalcontroller.getRegionais();
+    const atividades = await trabalhoscontroller.getAtividades();
 
-  await authcontroller.authenticate(req,res,'pages/pesquisar',
-  { 
-    regionais: regionais, 
-    atividades: atividades 
-  });
+    res.render("pages/pesquisar", {
+      regionais: regionais,
+      atividades: atividades,
+    });
+  }
 });
 
 app.post("/pesquisa", async function (req, res) {
-  let centro = []
-  try {
-    const regional = req.body.regional
-    const trabalho = req.body.trabalho
-    const search = req.body.search
-    const opcao = "Trabalho"
+  if (await TryAuthenticate(req, res)) {
+    let centro = [];
+    try {
+      const regional = req.body.regional;
+      const trabalho = req.body.trabalho;
+      const search = req.body.search;
+      const opcao = "Trabalho";
 
-    const pesquisaInfo ={
-      regional: regional,
-      trabalho: trabalho,
-      search: search,
-      option: opcao
-    }
+      const pesquisaInfo = {
+        regional: regional,
+        trabalho: trabalho,
+        search: search,
+        option: opcao,
+      };
+      const result = await searchcontroller.getPesquisaResult(pesquisaInfo);
 
-    console.log("PESQUISAR", regional, trabalho, search)
+      if (result) {
+        res.render(req, res, "pages/pesquisa", {
+          opcao: opcao,
+          result: result,
+        });
+      } else {
+        const opcoes = ["Centro", "Regional", "Trabalho"];
 
-    const result = await searchcontroller.getPesquisaResult(pesquisaInfo);
-
-    console.log("RESULT",result)
-    if(result){
-      await authcontroller.authenticate(req,res,'pages/pesquisa',{ opcao: opcao, result: result });
-    }
-    else{
-      const opcoes = [
-        "Centro",
-        "Regional",
-        "Trabalho"
-      ]
-
-      
-
-      await authcontroller.authenticate(req,res,'pages/pesquisar',{ opcoes: opcoes, result: null });
-    }
-  } catch (error) {
-    
+        res.render(req, res, "pages/pesquisar", {
+          opcoes: opcoes,
+          result: null,
+        });
+      }
+    } catch (error) {}
+    console.log(centro);
   }
-  console.log(centro);
-
 });
 
 // about page
