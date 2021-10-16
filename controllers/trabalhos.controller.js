ef8ddfb734fb71daeb0d6fbc11c1ace005b483af
Bruno Duarte <bruno.duartec@gmail.com>
2021-10-16 12:37:47 -0300

    Adapt to use generic form

--- controllers/trabalhos.controller.js
+++ controllers/trabalhos.controller.js
@@ -8,22 +8,139 @@ module.exports = class trabalhosController {
 
   async getAtividades() {
     try {
-      const atividades = await axios.get(`${config.aee_digital_trabalhos}/atividades`);
-      console.log("atividades.controller.getAtividades: Success=>", atividades.data)      
+      const atividades = await axios.get(
+        `${config.aee_digital_trabalhos}/atividade`
+      );
+      console.log(
+        "trabalhos.controller.getAtividades: Success=>",
+        atividades.data
+      );
       return atividades.data;
     } catch (error) {
-      console.log("trabalhos.controller.getAtividades: Error=>", error)      
+      console.log("trabalhos.controller.getAtividades: Error=>", error);
     }
   }
 
   async getAtividadesCentroByParams(params) {
     try {
-      const atividades = await axios.get(`${config.aee_digital_trabalhos}/atividades_centro?${params}`);
-      console.log("atividades.controller.getAtividades: Success=>", atividades.data)      
+      const atividades = await axios.get(
+        `${config.aee_digital_trabalhos}/atividade_centro?${params}`
+      );
+      console.log(
+        "trabalhos.controller.getAtividades: Success=>",
+        atividades.data
+      );
       return atividades.data;
     } catch (error) {
-      console.log("trabalhos.controller.getAtividades: Error=>", error)      
+      console.log("trabalhos.controller.getAtividades: Error=>", error);
     }
   }
 
+  async getAtividadesCentroSummaryByParams(params) {
+    try {
+      const atividades = await axios.get(
+        `${config.aee_digital_trabalhos}/atividade_centro_summary?${params}`
+      );
+      console.log(
+        "trabalhos.controller.getAtividadesCentroSummaryByParams: Success=>",
+        atividades.data
+      );
+      return atividades.data;
+    } catch (error) {
+      console.log(
+        "trabalhos.controller.getAtividadesCentroSummaryByParams: Error=>",
+        error
+      );
+    }
+  }
+
+  async getFormByParams(params) {
+    try {
+      const form = await axios.get(
+        `${config.aee_digital_trabalhos}/atividade_generic_form?${params}`
+      );
+      console.log("trabalhos.controller.getFormByParams: Success=>", form.data);
+      return form.data;
+    } catch (error) {
+      console.log("trabalhos.controller.getFormByParams: Error=>", error);
+    }
+  }
+
+  async getQuizTemplateByParams(params) {
+    try {
+      const quiz = await axios.get(
+        `${config.aee_digital_trabalhos}/atividade_generic_quiz?${params}`
+      );
+      console.log(
+        "trabalhos.controller.getQuizTemplateByParams: Success=>",
+        quiz.data
+      );
+      return quiz.data;
+    } catch (error) {
+      console.log(
+        "trabalhos.controller.getQuizTemplateByParams: Error=>",
+        error
+      );
+    }
+  }
+
+  async getQuizResponseByParams(params) {
+    try {
+      const quiz_response = await axios.get(
+        `${config.aee_digital_trabalhos}/atividade_generic_quiz_answer?${params}`
+      );
+      console.log(
+        "trabalhos.controller.getQuizResponseByParams: Success=>",
+        quiz_response.data
+      );
+      return quiz_response.data;
+    } catch (error) {
+      console.log(
+        "trabalhos.controller.getQuizResponseByParams: Error=>",
+        error
+      );
+    }
+  }
+
+  async putQuizResponse(params, value) {
+    console.log(params);
+
+    try {
+      const quiz_response = await axios.put(
+        `${config.aee_digital_trabalhos}/atividade_generic_quiz_answer?${params}`,
+        value
+      );
+      console.log(
+        "trabalhos.controller.getQuizResponseByParams: Success=>",
+        quiz_response.data
+      );
+      return quiz_response.data;
+    } catch (error) {
+      console.log(
+        "trabalhos.controller.getQuizResponseByParams: Error=>",
+        error
+      );
+    }
+  }
+
+  async postQuizResponse(params) {
+    console.log(params);
+
+    try {
+      const quiz_response = await axios.post(
+        `${config.aee_digital_trabalhos}/atividade_generic_quiz_answer`,
+        params
+      );
+      console.log(
+        "trabalhos.controller.getQuizResponseByParams: Success=>",
+        quiz_response.data
+      );
+      return quiz_response.data;
+    } catch (error) {
+      console.log(
+        "trabalhos.controller.getQuizResponseByParams: Error=>",
+        error
+      );
+    }
+  }
 };
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const config = require("./env.json")["local"];
 const express = require("express");
 const app = express();
 var session = require("express-session");
+// const cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+
+const crypto = require("crypto");
 
 const regionalController = require("./controllers/regional.controller");
 const regionalcontroller = new regionalController();
@@ -19,8 +22,14 @@ const searchcontroller = new SearchController(
   trabalhoscontroller
 );
 
+// This will hold the users and authToken related to users
+const authTokens = {};
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+// app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(
   session({
@@ -29,8 +38,29 @@ app.use(
     saveUninitialized: true,
   })
 );
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
+const generateAuthToken = () => {
+  return crypto.randomBytes(30).toString("hex");
+};
+
+app.use((req, res, next) => {
+  // Get auth token from the cookies
+  const authToken = req.session.authToken;
+
+  // Inject the user to the request
+  req.user = authTokens[authToken];
+
+  next();
+});
+
+const requireAuth = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    req.session.originalUrl = req.originalUrl;
+    res.render("pages/login");
+  }
+};
 
 async function TryAuthenticate(req, res, route) {
   const loginInfo = {
@@ -38,132 +68,250 @@ async function TryAuthenticate(req, res, route) {
     pass: req.body.pass,
   };
 
-  req.body.route = req.originalUrl;
-  if (!req.session.loggedin) {
-    const auth = await authcontroller.authenticate(loginInfo);
-    if (!auth) {
-      res.render("pages/login", { auth: false, ...loginInfo });
-      return false;
-    } else {
-      req.session.loggedin = true;
-      return true;
-    }
+  const auth = await authcontroller.authenticate(loginInfo);
+  if (!auth) {
+    res.redirect("/login");
   } else {
-    return true;
+    const authToken = generateAuthToken();
+    authTokens[authToken] = loginInfo.user;
+
+    req.session.authToken = authToken;
+
+    if (req.session.originalUrl) {
+      res.redirect(req.session.originalUrl);
+    } else {
+      res.render("pages/index");
+    }
   }
 }
 
-app.get("/", async function (req, res) {
-  if (await TryAuthenticate(req, res)) {
-    res.render("pages/index");
+app.get("/", requireAuth, async function (req, res) {
+  res.render("pages/index");
+});
+
+app.get("/centro", requireAuth, async function (req, res) {
+  const centro = req.query.nome;
+  const edit = req.query.edit;
+  const option = "Centro";
+
+  const pesquisaInfo = {
+    search: centro,
+    option: option,
+  };
+  const result = await searchcontroller.getPesquisaResult(pesquisaInfo);
+
+  if (edit) {
+    res.render("pages/editar", {
+      opcao: option,
+      result: result.items,
+    });
+  } else {
+    res.render("pages/detalhe", {
+      opcao: option,
+      result: result.items,
+    });
   }
 });
 
-app.get("/centro", async function (req, res) {
-  if (await TryAuthenticate(req, res)) {
-    const centro = req.query.nome;
-    const edit = req.query.edit;
-    const option = "Centro";
+app.get("/trabalho_centro", requireAuth, async function (req, res) {
+  const centro = req.query.nome;
+  const edit = req.query.edit;
+  const option = "Centro_Summary";
 
-    const pesquisaInfo = {
-      search: centro,
-      option: option,
-    };
-    const result = await searchcontroller.getPesquisaResult(pesquisaInfo);
+  const pesquisaInfo = {
+    centro: centro,
+    trabalho: "Todos",
+    option: option,
+  };
+  const result = await searchcontroller.getPesquisaResult(pesquisaInfo);
 
-    if (edit) {
-      res.render("pages/editar_centro", { result: result });
-    } else {
-      res.render("pages/detalhe", {
-        opcao: option,
-        result: result,
-      });
+  if (edit) {
+    res.render("pages/editar", {
+      opcao: "Trabalhos",
+      result: result.items,
+    });
+  } else {
+    res.render("pages/detalhe", {
+      opcao: option,
+      result: result.items,
+    });
+  }
+});
+
+app.get("/cadastro", requireAuth, async function (req, res) {
+  const centro = req.query.nome;
+  const edit = req.query.edit;
+  const option = "Centro_Summary";
+
+  const pesquisaInfo = {
+    centro: centro,
+    trabalho: "Todos",
+    option: option,
+  };
+  const result = await searchcontroller.getPesquisaResult(pesquisaInfo);
+
+  if (edit) {
+    res.render("pages/editar", {
+      opcao: "Trabalhos",
+      result: result.items,
+    });
+  } else {
+    res.render("pages/detalhe", {
+      opcao: option,
+      result: result.items,
+    });
+  }
+});
+
+async function getFormInfo(centro_id, form_alias) {
+  let option = "Centro";
+
+  let pesquisaInfo = {
+    search: centro_id,
+    option: option,
+  };
+
+  option = "Quiz";
+
+  pesquisaInfo = {
+    search: {
+      id: centro_id,
+      name: form_alias,
+    },
+    option: option,
+  };
+  const result = await searchcontroller.getPesquisaResult(pesquisaInfo);
+  return result;
+}
+
+app.get("/cadastro_alianca", requireAuth, async function (req, res) {
+  const centro_id = req.query.ID;
+  const  page = req.query.page || 1;
+  const form_alias = "Cadastro de Informações Anual";
+
+  const form_info = await getFormInfo(centro_id, form_alias);
+
+  res.render("pages/quiz", {
+    index: page,
+    form_alias: form_alias,
+    centro_id: centro_id,
+    results: form_info.templates,
+    titles: form_info.titles
+  });
+});
+
+app.post("/quiz", requireAuth, async function (req, res) {
+  let responses = req.body;
+  const form_alias = responses.form_alias;
+  const centro_id = responses.centro_id;
+
+  const form_info = await getFormInfo(centro_id, form_alias);
+
+  const quizes = form_info.templates.QUIZES;
+
+  for (let index = 0; index < quizes.length; index++) {
+    const quiz = quizes[index];
+    const questions = quiz.QUESTIONS;
+
+    for (let j = 0; j < questions.length; j++) {
+      const question = questions[j];
+
+      let answer = responses[question._id];
+      if(responses[question._id] == "on") answer = "true"
+
+      if (responses[question._id]) {
+        if (!question.ANSWER) {
+          await trabalhoscontroller.postQuizResponse({
+            CENTRO_ID: centro_id,
+            QUIZ_ID: quiz._id,
+            QUESTION_ID: question._id,
+            ANSWER: answer,
+          });
+        } else if (question.ANSWER != responses[question._id]) {
+          let paramsParsed = searchcontroller.getParamsParsed({
+            CENTRO_ID: centro_id,
+            QUIZ_ID: quiz._id,
+            QUESTION_ID: question._id,
+          });
+          await trabalhoscontroller.putQuizResponse(paramsParsed, {
+            ANSWER: answer,
+          });
+        }
+      }
     }
   }
 });
 
-app.post("/update_centro", async function (req, res) {
-  if (await TryAuthenticate(req, res)) {
-    const centroInfo = {
-      NOME_CENTRO: req.body.nome,
-      NOME_CURTO: req.body.NOME_CURTO,
-      CNPJ_CENTRO: req.body.cnpj,
-      DATA_FUNDACAO: req.body.fundacao,
-      ENDERECO: req.body.endereco,
-      CEP: req.body.cep,
-      BAIRRO: req.body.bairro,
-      CIDADE: req.body.cidade,
-      ESTADO: req.body.estado,
-      PAIS: req.body.pais,
-    };
+app.post("/update_centro", requireAuth, async function (req, res) {
+  const centroInfo = {
+    NOME_CENTRO: req.body.nome,
+    NOME_CURTO: req.body.NOME_CURTO,
+    CNPJ_CENTRO: req.body.cnpj,
+    DATA_FUNDACAO: req.body.fundacao,
+    ENDERECO: req.body.endereco,
+    CEP: req.body.cep,
+    BAIRRO: req.body.bairro,
+    CIDADE: req.body.cidade,
+    ESTADO: req.body.estado,
+    PAIS: req.body.pais,
+  };
 
-    res.render("pages/detalhe", {
-      opcao: option,
-      result: result,
-    });
-  }
+  res.render("pages/detalhe", {
+    opcao: option,
+    result: result,
+  });
 });
 
 app.get("/login", async function (req, res) {
-  if (await TryAuthenticate(req, res)) {
-    const route = req.body.route;
-    res.render("pages/index");
-  }
+  res.render("pages/login");
 });
 
 app.post("/login", async function (req, res) {
-  if (await TryAuthenticate(req, res)) {
-    const route = req.body.route;
-    // res.redirect(route);
-    res.render("pages/index");
-  }
+  await TryAuthenticate(req, res);
 });
 
-app.get("/pesquisar", async function (req, res) {
-  if (await TryAuthenticate(req, res)) {
-    const regionais = await regionalcontroller.getRegionais();
-    const atividades = await trabalhoscontroller.getAtividades();
 
-    res.render("pages/pesquisar", {
-      regionais: regionais,
-      atividades: atividades,
-    });
-  }
+app.get("/pesquisar", requireAuth, async function (req, res) {
+  const regionais = await regionalcontroller.getRegionais();
+  const atividades = await trabalhoscontroller.getAtividades();
+
+  res.render("pages/pesquisar", {
+    regionais: regionais,
+    atividades: atividades,
+  });
 });
 
-app.post("/pesquisa", async function (req, res) {
-  if (await TryAuthenticate(req, res)) {
-    let centro = [];
-    try {
-      const regional = req.body.regional;
-      const trabalho = req.body.trabalho;
-      const search = req.body.search;
-      const opcao = "Trabalho";
+app.post("/pesquisa", requireAuth, async function (req, res) {
+  let centro = [];
+  try {
+    const regional = req.body.regional;
+    const trabalho = req.body.trabalho;
+    const search = req.body.search;
+    const opcao = "Trabalho";
 
-      const pesquisaInfo = {
-        regional: regional,
-        trabalho: trabalho,
-        search: search,
-        option: opcao,
-      };
-      const result = await searchcontroller.getPesquisaResult(pesquisaInfo);
+    const pesquisaInfo = {
+      regional: regional,
+      trabalho: trabalho,
+      search: search,
+      option: opcao,
+    };
+    const result = await searchcontroller.getPesquisaResult(pesquisaInfo);
 
-      if (result) {
-        res.render("pages/pesquisa", {
-          opcao: opcao,
-          result: result,
-        });
-      } else {
-        const opcoes = ["Centro", "Regional", "Trabalho"];
+    if (result) {
+      res.render("pages/pesquisa", {
+        opcao: opcao,
+        result: result,
+      });
+    } else {
+      const opcoes = ["Centro", "Regional", "Trabalho"];
 
-        res.render("pages/pesquisar", {
-          opcoes: opcoes,
-          result: null,
-        });
-      }
-    } catch (error) {}
-    console.log(centro);
-  }
+      res.render("pages/pesquisar", {
+        opcoes: opcoes,
+        result: null,
+      });
+    }
+  } catch (error) {}
+  console.log(centro);
 });
 
 // about page
